d60d508e1554defd322e849c80d218407fe544a5
Pjdoranbrown <paddy84@gmail.com>
2026-02-16 09:41:49 +1100

    Create js/config.js file with configuration constants

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {/* your config values */};
+const RACES = {/* your race definitions */};
+const NAMES = {/* your name definitions */};
+const CLASS_DEFS = {/* your class definitions */};
+const ENEMY_DEFS = {/* your enemy definitions */};
+const UPGRADES = {/* your upgrade definitions */};
+
+export { CONFIG, RACES, NAMES, CLASS_DEFS, ENEMY_DEFS, UPGRADES };
